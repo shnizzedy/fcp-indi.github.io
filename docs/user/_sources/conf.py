2c2711d448970c1db681ccd6cf80624361f1841a
Jon Clucas <jon.clucas@childmind.org>
2020-05-28 17:07:39 -0400

    :construction: Move "Installing C-PAC" from user to dev docs

--- docs/user/_sources/conf.py
+++ docs/user/_sources/conf.py
@@ -33,10 +33,17 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.ifconfig',
-              'sphinx.ext.intersphinx', 'sphinx.ext.viewcode',
-              'sphinx.ext.mathjax', 'sphinxcontrib.programoutput', 'exec',
-              'numpydoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.extlinks',
+              'sphinx.ext.ifconfig', 'sphinx.ext.intersphinx',
+              'sphinx.ext.viewcode', 'sphinx.ext.mathjax',
+              'sphinxcontrib.programoutput', 'exec', 'numpydoc']
+
+extlinks = {
+    'cpac_dev': (
+        'https://fcp-indi.github.io/docs/developer/%s',
+        'Developer documentation: '
+    )
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
