f31112393427252112b8366a659a225675a352d4
ChildMindInstituteCNL <CMI_CPAC_Support@childmind.org>
2023-10-04 17:11:25 +0000

    :art: Hide empty `<div><div><pre><span>` elements

--- docs/nightly/_static/hideEmptyPreSpan.js
+++ docs/nightly/_static/hideEmptyPreSpan.js
@@ -1,10 +1,15 @@
-/* Hide pre elements containing only empty spans */
+/* Hide containers of pre elements containing only empty spans */
 document.addEventListener("DOMContentLoaded", function() {
   const preElements = document.querySelectorAll('pre');
   preElements.forEach(function(preElement) {
     const childElements = preElement.children;
     if (childElements.length === 1 && childElements[0].tagName === 'SPAN' && childElements[0].textContent.trim() === '') {
-      preElement.classList.add('hidden');
+      const parentElement = preElement.parentElement;
+      const grandparentElement = parentElement.parentElement;
+      if (parentElement && parentElement.tagName === 'DIV' &&
+          grandparentElement && grandparentElement.tagName === 'DIV') {
+        grandparentElement.classList.add('hidden');
+      }
     }
   });
 });
