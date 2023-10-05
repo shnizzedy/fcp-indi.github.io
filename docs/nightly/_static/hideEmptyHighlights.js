478734892e6f8ee63aaf4fa3c61c811aecfec07d
ChildMindInstituteCNL <CMI_CPAC_Support@childmind.org>
2023-10-05 01:45:59 +0000

    :art: Hide empty content `<div class="highlight-*">` elements

--- docs/nightly/_static/hideEmptyHighlights.js
+++ docs/nightly/_static/hideEmptyHighlights.js
@@ -1,6 +1,6 @@
 /* Hide highlight containers only empty spans */
 document.addEventListener("DOMContentLoaded", function() {
-  const highlightDivs = document.querySelectorAll('div[class^="highlight-"], div[class*=" highlight-"]');
+  const highlightDivs = document.querySelectorAll('div[class^="highlight"], div[class*=" highlight"]');
   highlightDivs.forEach(function(highlightDiv) {
     if (highlightDiv.textContent.trim() === '') {
       highlightDiv.classList.add('hidden');
